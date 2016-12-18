2df2adda98be8c47bd780fc1c09e6ff52439dcd0
mkarmowski <mateusz@karmowski.com>
2016-12-18 11:58:30 +0100

    updated cart

--- cart/cart.py
+++ cart/cart.py
@@ -25,7 +25,6 @@ class Cart(object):
         products = Product.objects.filter(id__in=product_ids)
         for product in products:
             self.cart[str(product.id)]['product'] = product
-
         for item in self.cart.values():
             item['price'] = Decimal(item['price'])
             item['total_price'] = item['price'] * item['quantity']
