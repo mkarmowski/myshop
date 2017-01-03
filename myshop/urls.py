3948ff8e491fc6a6db0719ababb674043a4950af
mkarmowski <mateusz@karmowski.com>
2017-01-03 20:45:40 +0100

    edited views to accept translations

--- myshop/urls.py
+++ myshop/urls.py
@@ -3,14 +3,15 @@ from django.conf.urls import include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 urlpatterns = i18n_patterns(
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^cart/', include('cart.urls', namespace='cart')),
-    url(r'^orders/', include('orders.urls', namespace='orders')),
-    url(r'^payment/', include('payment.urls', namespace='payment')),
+    url(_(r'^cart/'), include('cart.urls', namespace='cart')),
+    url(_(r'^orders/'), include('orders.urls', namespace='orders')),
+    url(_(r'^payment/'), include('payment.urls', namespace='payment')),
     url(r'^paypal/', include('paypal.standard.ipn.urls')),
-    url(r'^coupons/', include('coupons.urls', namespace='coupons')),
+    url(_(r'^coupons/'), include('coupons.urls', namespace='coupons')),
     url(r'^rosetta/', include('rosetta.urls')),
     url(r'^', include('shop.urls', namespace='shop')),
 )
