8c92d44b481f7ed0c8027caa2c5c72bf580748fa
mkarmowski <mateusz@karmowski.com>
2017-01-03 19:41:04 +0100

    created custom migrations for translated models

--- shop/models.py
+++ shop/models.py
@@ -4,8 +4,6 @@ from parler.models import TranslatableModel, TranslatedFields
 
 
 class Category(TranslatableModel):
-    name = models.CharField(max_length=200, db_index=True)
-    slug = models.CharField(max_length=200, db_index=True, unique=True)
     translations = TranslatedFields(
         name = models.CharField(max_length=200, db_index=True),
         slug = models.SlugField(max_length=200, db_index=True, unique=True)
@@ -24,21 +22,18 @@ class Category(TranslatableModel):
 
 
 class Product(TranslatableModel):
-    category = models.ForeignKey(Category, related_name='products')
-    name = models.CharField(max_length=200, db_index=True)
-    slug = models.CharField(max_length=200, db_index=True)
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
-    description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.PositiveIntegerField()
-    available = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
     translations = TranslatedFields(
         name = models.CharField(max_length=200, db_index=True),
         slug = models.SlugField(max_length=200, db_index=True),
         description = models.TextField(blank=True)
     )
+    category = models.ForeignKey(Category, related_name='products')
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ('-created',)
